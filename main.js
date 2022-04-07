e996a2909b3bd69d784ae03d3d233ae00c537bb2
student1432 <84581003+student1432@users.noreply.github.com>
2022-04-07 15:42:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+function draw()
+{
+
+}
+function takeSnapshot()
+{
+    save('myFilterSelfie.png');
+}
